f47542fee061ae32ffa3d184f269b2673b8ea81e
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-11-03 22:28:33 +1100

    start adding structs for device info and capabilities request messages

--- src/message.rs
+++ src/message.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 // A message must implement the following functions
 pub trait Message {
   /** Returns Some for expected data in bytes and None when the data length is variable */
@@ -7,15 +9,13 @@ pub trait Message {
 pub trait Command {}
 pub trait Reply {}
 
-
 /*
-  Basic Commands
-  ==============
+  Commands
+  ========
 */
 
-pub struct Poll {}          // 0x60
-pub struct ReaderLED {}     // 0x69
-
+pub struct Poll {} // 0x60
+pub struct ReaderLED {} // 0x69
 
 impl Message for Poll {
   fn data_length(&self) -> Option<i32> {
@@ -23,17 +23,62 @@ impl Message for Poll {
   }
 }
 
+pub struct DeviceIDReportRequest {}
+impl DeviceIDReportRequest {
+  fn serialise(&self) -> Vec<u8> {
+    Vec::from([0x00])
+  }
+}
+
+pub struct DeviceCapabilitiesRequest {}
+impl DeviceCapabilitiesRequest {
+  fn serialise(&self) -> Vec<u8> {
+    Vec::from([0x00])
+  }
+}
 
 /*
-  Basic Replies
-  ==============
+  Replies
+  =======
 */
 
-pub struct Ack {}           // 0x40
-pub struct Nack {}          // 0x41
+pub struct Ack {} // 0x40
+pub struct Nack {} // 0x41
 
 impl Message for Ack {
   fn data_length(&self) -> Option<i32> {
     Some(0)
+  }
+}
+
+#[derive(Debug)]
+pub struct DeviceIDReport {
+  vendor_code: u32,
+  model_no: u8,
+  model_version: u8,
+  serial_number: u32,
+  firmware_version: String,
+}
+
+struct Capability {
+  function_code: u8,
+  compliance: u8,
+  number_of: u8
+}
+pub struct DeviceCapabilitiesReport {
+  capabilities: Vec<Capability>
+}
+
+impl fmt::Display for DeviceIDReport {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    write!(
+      f,
+      "Vendor Code: {}\nModel Number: {}\nModel Version: {}\nSerial Number: {}\nFirmware: {}\n",
+      self.vendor_code,
+      self.model_no,
+      self.model_version,
+      self.serial_number,
+      self.firmware_version
+    )
   }
 }
