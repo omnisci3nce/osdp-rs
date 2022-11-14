21e4ca6cb7a4fe7a87ae6c4700a8f1bd94c7709a
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-11-14 20:24:53 +1100

    test send device id request and parse and deserialise response

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,69 @@
-use osdp_rs::message::{Message, Poll};
+use osdp_rs::crc::calc_checksum;
+use osdp_rs::message::from_packet;
 use osdp_rs::packet::Packet;
 use osdp_rs::parser::Parser;
+use std::io;
+use std::time::Duration;
 
 fn main() {
   println!("osdp-rs");
 
   let mut parser = Parser::new(); // instantiate default parser state
 
-  //                            SOM   ADDR  LEN   LEN   CTRL c(POLL) CRC1  CRC2
-  let raw_bytes: Vec<u8> = vec![0x53, 0x80, 0x08, 0x00, 0x04, 0x60, 0x00, 0x00];
+  let builder = serialport::new("/dev/ttyUSB0", 9600).timeout(Duration::from_millis(20));
+  println!("{:?}", &builder);
+  let mut port = builder.open().expect("Failed to open port");
 
-  for byte in raw_bytes {
-    let completed_packet: Option<Packet> = parser.parse_byte(byte);
-    match completed_packet {
-      Some(p) => println!("Complete packet received: {:?}", p),
-      None => (),
+  // Create a packet for testing (requests device info)
+  // -------------------------------------------------
+  let mut packet = Vec::with_capacity(20);
+
+  // create header       SOM   ADDR  LEN -----------|  CTRL
+  let len: u16 = (5 + 1 + 1 + 1);
+  let len_lsb = (len & 0xFF) as u8;
+  let len_msb = ((len >> 8) & 0xFF) as u8;
+  let header: [u8; 5] = [0x53, 0x00, len_lsb, len_msb, 0x00];
+  for b in header {
+    packet.push(b);
+  }
+  // command type
+  packet.push(0x61);
+  // data block
+  let data_block = Vec::from([0x00]);
+  for b in data_block {
+    packet.push(b);
+  }
+  // validation
+  let chksum = calc_checksum(&packet);
+  packet.push(chksum);
+  // -------------------------------------------------
+
+  let mut read_buffer: [u8; 1] = [0];
+  port.write(&packet).expect("Write failed!");
+  loop {
+    match port.read(&mut read_buffer) {
+      Ok(bytes) => {
+        let byte = read_buffer[0]; // the byte we just read in
+        let maybe_completed_packet: Option<Packet> = parser.parse_byte(byte);
+        match maybe_completed_packet {
+          Some(p) => {
+            println!("Complete packet received: {:?}", p);
+            // Attempt to deserialise it
+            let msg = from_packet(p);
+            // TODO: Remove a lot of this match nesting
+            match msg {
+              Ok(msg) => match msg {
+                osdp_rs::message::Message::REPLY_PDID(d) => println!("{}", d),
+                _ => (),
+              },
+              Err(_e) => panic!("Error deserialising packet to message!"),
+            };
+          }
+          None => (),
+        }
+      }
+      Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+      Err(e) => eprintln!("{:?}", e),
     }
   }
 }
