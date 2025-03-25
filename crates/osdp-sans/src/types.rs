60d1aecb2297851576fc42d0e82b0c6eadf82fd4
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2025-03-26 00:41:03 +1100

    remote notes.md

--- crates/osdp-sans/src/types.rs
+++ crates/osdp-sans/src/types.rs
@@ -1,2 +1,8 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Address(pub u8);
+
+/// Represents a device on the RS485 bus and any information
+/// we have about it.
+pub struct BusDevice {
+    address: Address,
+}
